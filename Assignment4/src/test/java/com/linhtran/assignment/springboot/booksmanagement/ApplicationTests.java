4fbeb9264fd54f1156e3633edaf65657dac01f53
tranmanhlinh <manhlinh.sama@gmail.com>
2018-03-29 21:50:11 +0700

    Initialize an example, some error in controller

---
+++ Assignment4/src/test/java/com/linhtran/assignment/springboot/booksmanagement/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.linhtran.assignment.springboot.booksmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
