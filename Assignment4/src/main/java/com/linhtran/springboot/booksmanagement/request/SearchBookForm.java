25765a6217729b14af45453edf8724068f301865
tranmanhlinh <manhlinh.sama@gmail.com>
2018-04-11 15:24:16 +0700

    Done searching feature.

---
+++ Assignment4/src/main/java/com/linhtran/springboot/booksmanagement/request/SearchBookForm.java
@@ -0,0 +1,22 @@
+package com.linhtran.assignment.booksmanagement.model;
+
+public class SearchBookForm {
+    private String searchType;
+    private String searchValue;
+
+    public String getSearchType() {
+        return searchType;
+    }
+
+    public void setSearchType(String searchType) {
+        this.searchType = searchType;
+    }
+
+    public String getSearchValue() {
+        return searchValue;
+    }
+
+    public void setSearchValue(String searchValue) {
+        this.searchValue = searchValue;
+    }
+}
